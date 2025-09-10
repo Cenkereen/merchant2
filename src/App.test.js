fa269e11dea8e0547879f6c2938698a8c406b39e
Cenkereen <cenk.eren@std.yeditepe.edu.tr>
2025-09-10 12:29:45 +0300

    Initial commit of React frontend

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Merchant Login heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Check that the heading "Merchant Login" exists
+  const headingElement = screen.getByText(/Merchant Login/i);
+  expect(headingElement).toBeInTheDocument();
 });
